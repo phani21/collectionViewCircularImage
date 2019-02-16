c506c7ef680f955d061b5193c8b86e17f39fd853
Phani_Kiran <pret321@gmail.com>
2019-02-15 23:11:24 -0600

    commit

---
+++ collectionViewCircularImage/CollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  CollectionViewCell.swift
+//  collectionViewCircularImage
+//
+//  Created by IMCS2 on 2/15/19.
+//  Copyright © 2019 com.phani. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cvImg: UIImageView!
+    @IBOutlet weak var cvlabel: UITextField!
+    
+//
+//    let tapGesture = UITapGestureRecognizer(target: self, action: "imageTapped:")
+//
+//    // add it to the image view;
+//    cvImg.addGestureRecognizer(tapGesture)
+//    // make sure imageView can be interacted with by user
+//    cvImg.userInteractionEnabled = true
+//}
+//
+//func imageTapped(gesture: UIGestureRecognizer) {
+//    // if the tapped view is a UIImageView then set it to imageview
+//    if let cvImg = gesture.view as? UIImageView {
+//        print("Image Tapped")
+//        //Here you can initiate your new ViewController
+    
+    }
+
+    
+    
+    
+
--- collectionViewCircularImage/ViewController.swift
+++ collectionViewCircularImage/ViewController.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
+    var i : Int = 0
+class ViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    let labels = ["1","2","3","4","5","6","7","8","9","10"]
+    let imgs :[UIImage?]=[
+    UIImage(named: "one")!,
+    UIImage(named: "two")!,
+    UIImage(named: "three")!,
+    UIImage(named: "four")!,
+    UIImage(named: "five")!,
+    UIImage(named: "six")!,
+    UIImage(named: "seven")!,
+    UIImage(named: "eight")!,
+    UIImage(named: "nine")!,
+    UIImage(named: "ten")!,
+    
+    
+    ]
 
-class ViewController: UIViewController {
+    @IBOutlet weak var cv: UICollectionView!
+    
+    
+    @IBOutlet weak var display: UIImageView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return labels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=cv.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+         cell.cvlabel.text=labels[indexPath.item]
+        cell.cvImg.image=imgs[indexPath.item]
+        
+        print(indexPath)
+        i=indexPath.max()!
+  
+        
+        if(indexPath.item>8){
+           
+            return cell
+        }
+        
+        
+        return cell
+        
+    }
+    
 
+
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        display.image=imgs[indexPath.item]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
